2dce12cdc6ac4a12b5035ba7ff8316ed5f81d246
jonaskahn <tuyendev@gmail.com>
2024-11-19 23:30:27 +0700

    add deployment maven central

--- src/main/kotlin/one/ifelse/tools/useragent/RandomUserAgent.kt
+++ src/main/kotlin/one/ifelse/tools/useragent/RandomUserAgent.kt
@@ -4,10 +4,21 @@ import one.ifelse.tools.useragent.types.UserAgent
 
 object RandomUserAgent {
 
+    /**
+     * Get a random user-agent from all available.
+     *
+     * @return a random user-agent
+     */
     fun random(): String {
         return Seeds.USER_AGENTS.random().userAgent
     }
 
+    /**
+     * Get a random user-agent from all available, filtered by given predicates.
+     *
+     * @param predicates predicates to filter user-agents
+     * @return a random user-agent that matches all predicates
+     */
     fun random(vararg predicates: (UserAgent) -> Boolean): String {
         return Seeds.USER_AGENTS
             .filter { item -> predicates.all { predicate -> predicate(item) } }
